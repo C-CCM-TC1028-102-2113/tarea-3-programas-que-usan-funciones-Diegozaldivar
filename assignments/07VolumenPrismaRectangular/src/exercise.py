577512624a754064fd477ab2ee27383ca14f1840
Diegozaldivar <89473869+Diegozaldivar@users.noreply.github.com>
2021-08-30 21:59:30 -0500

    Update exercise.py

--- assignments/07VolumenPrismaRectangular/src/exercise.py
+++ assignments/07VolumenPrismaRectangular/src/exercise.py
@@ -1,7 +1,34 @@
 
-def main():
-    #escribe tu código abajo de esta línea
-    pass
+#Volumen de un prisma rectangular
 
-if __name__=='__main__':
+def rectangulo(base,altura):
+    area = base * altura
+    return area
+    
+
+
+def prisma(AREA_TOTAL, ancho):
+    volumen = AREA_TOTAL * ancho
+    print( 'El volumen del prisma es: '+ str(volumen))
+
+
+
+def main():
+  #escribe tu código abajo de esta línea+
+    base = float(input('Dame la base:'))
+    altura = float(input('Dame la altura:'))
+    ancho = float(input('Dame el ancho:'))
+    
+    AREA_TOTAL = rectangulo(base,altura)
+    print('El area del rectángulo es:' + str(AREA_TOTAL))
+
+    prisma(AREA_TOTAL, ancho)
+
+
+
+
+
+if __name__ == '__main__':
     main()
+    
+    
