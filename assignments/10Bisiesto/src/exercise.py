2719b0f03988d6d6e7bcb6f3bc13cc2c7305865e
Diegozaldivar <89473869+Diegozaldivar@users.noreply.github.com>
2021-08-30 22:27:01 -0500

    Update exercise.py

--- assignments/10Bisiesto/src/exercise.py
+++ assignments/10Bisiesto/src/exercise.py
@@ -1,7 +1,21 @@
 
-def main():
-    #escribe tu código abajo de esta línea
-    pass
+#Año bisiesto
 
-if __name__=='__main__':
+def main():
+  #escribe tu código abajo de esta línea
+    año = int(input('Ingresa el año:'))
+    if año % 4 != 0:
+     año = print(str('False'))
+    elif año % 4 == 0 and año % 100 != 0:
+     año = print(str('True'))
+    elif año % 4 == 0 and año % 100 == 0 and año % 400 != 0:
+     año = print(str('False'))
+    elif año % 4 == 0 and año % 100 == 0 and año % 400 == 0:
+     año = print(str('True'))
+
+    
+    
+    
+
+if __name__ == '__main__':
     main()
